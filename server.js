079157dd63bdc42f12066326d1d9f4e32424f3b9
Kaustav <kaustavbhattacharya101@gmail.com>
2020-12-26 20:14:48 +0530

    made change

--- server.js
+++ server.js
@@ -47,7 +47,11 @@ mongoose.connection.on('connected', () => {
 })
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req,res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    })
 }
 
 app.use(morgan('tiny'));
